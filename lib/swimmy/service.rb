606fbac0b75e467d45bb7cc6cb7cc3ffcad8482c
KatakuraYasuhiro <katakura2022@s.okayama-u.ac.jp>
2022-05-12 11:25:28 +0900

    Add search_restaurant command

--- lib/swimmy/service.rb
+++ lib/swimmy/service.rb
@@ -2,10 +2,10 @@ module Swimmy
   module Service
     dir = File.dirname(__FILE__) + "/service"
 
-    autoload :Pokemoninfo, "#{dir}/pokemoninfo.rb"
-    autoload :Geocoding,   "#{dir}/geocoding.rb"
-    autoload :Weather,     "#{dir}/weather.rb"
-    autoload :Anniversary, "#{dir}/anniversary.rb"
+    autoload :Pokemoninfo,    "#{dir}/pokemoninfo.rb"
+    autoload :Geocoding,      "#{dir}/geocoding.rb"
+    autoload :Weather,        "#{dir}/weather.rb"
+    autoload :Anniversary,    "#{dir}/anniversary.rb"
     autoload :RestaurantInfo, "#{dir}/restaurant_info.rb"
   end
 end
